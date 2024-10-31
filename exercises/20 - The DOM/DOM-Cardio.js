a13b277f1c979c1aeb66ac1969f74b8803380231
Sarah Smith <155038730+sarahlibx@users.noreply.github.com>
2024-10-30 20:46:01 -0400

    The Dom Cardio

--- exercises/20 - The DOM/DOM-Cardio.js
+++ exercises/20 - The DOM/DOM-Cardio.js
@@ -1,23 +1,40 @@
 // Make a div
-
+const div = document.createElement('div');
 // add a class of wrapper to it
-
+div.classList.add('wrapper');
 // put it into the body
-
+document.body.appendChild(div);
 // make an unordered list
 
+const ul = `<ul>
+<li>one</li>
+<li>two</li>
+<li>three</li>
+</ul>`;
 // add three list items with the words "one, two, three" in them
 // put that list into the above wrapper
+div.innerHTML = ul;
 
 // create an image
-
+const img = document.createElement('img');
 // set the source to an image
+img.src = 'https://picsum.photos/500';
 // set the width to 250
+img.width = 250;
+img.height = 250;
 // add a class of cute
+img.classList.add('cute');
 // add an alt of Cute Puppy
+img.alt = 'cute puppy!';
 // Append that image to the wrapper
-
+div.appendChild(img);
 // with HTML string, make a div, with two paragraphs inside of it
+const myHTML = `
+  <div class="myDiv">
+    <p>Paragraph One</p>
+    <p>Paragraph Two</p>
+  </div>
+`;
 // put this div before the unordered list from above
 
 // add a class to the second paragraph called warning
--- playground/types-FINISHED.js
+++ playground/types-FINISHED.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 /*
-const name = 'wes';
+const firstName = 'wes';
 const middle = "topher";
 const last = `bos`;
 
@@ -14,17 +14,17 @@ ya
 I like
 pizza`;
 
-const hello = 'hello my name is ' + name + ". Nice to meet you";
+const hello = 'hello my name is ' + firstName + ". Nice to meet you";
 
 let hello2 = 'hello my name is ';
-hello2 = hello2 + name;
+hello2 = hello2 + firstName;
 hello2 = hello2 + ' Nice to meet you';
 
-const hello = `hello my name is ${name}. Nice to meet you. I am ${1 + 100} years old`;
+const hello = `hello my name is ${firstName}. Nice to meet you. I am ${1 + 100} years old`;
 
 const html = `
   <div>
-    <h2>${name}</h2>
+    <h2>${firstName}</h2>
     <p>${hello}</p>
   </div>
 `;
