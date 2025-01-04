372369a182d762cc6662dcea5f03cefe50a800ee
Sarah Smith <155038730+sarahlibx@users.noreply.github.com>
2025-01-04 13:12:26 -0500

    Currency Converter with API
    ** does not work, API key required now **

--- exercises/77 - Currency Converter/money.js
+++ exercises/77 - Currency Converter/money.js
@@ -1,3 +1,21 @@
+const fromSelect = document.querySelector('[name="from_currency"]');
+const toSelect = document.querySelector('[name="to_currency"]');
+const endpoint = 'https://api.exchangeratesapi.io/latest';
+const ratesByBase = {}; // this will store rates in it
+
+//api key
+const myHeaders = new Headers();
+myHeaders.append("apikey", "1Cf6EoC40n7hp44yHBGWr9aiiUKvcAOL");
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
+fetch(`https://api.apilayer.com/exchangerates_data/latest?symbols={symbols}&base={base}`, requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
 const currencies = {
   USD: 'United States Dollar',
   AUD: 'Australian Dollar',
@@ -32,3 +50,36 @@ const currencies = {
   ZAR: 'South African Rand',
   EUR: 'Euro',
 };
+
+function generateOptions(options) {
+  return Object.entries(options).map(([currencyCode, currencyName]) => 
+   `<option value="${currencyCode}">${currencyCode} - ${currencyName}</option>`
+  ).join('');
+}
+
+async function fetchRates(base = 'USD') {
+  const res = await fetch(`${endpoint}?base=${base}`);
+  const rates = await res.json();
+}
+
+async function convert(amount, from, to) {
+  //first check if we have the rates to convert from
+  if(!ratesByBase[from]) {
+    console.log(`oh no, we don't have ${from} to convert to ${to}. Let's go get it`);
+    const rates = await fetchRates(from);
+    console.log(rates);
+    //store rates for next time
+    ratesByBase[from] = rates;
+  }
+  //convert amount passed in
+  const rate = ratesByBase[from].rates[to];
+  const convertedAmount = rate * amount;
+  console.log(`${amount} ${from} is ${convertedAmount} in ${to}`);
+  return convertedAmount;
+}
+
+const optionsHTML = generateOptions(currencies);
+
+// on page load, populate options
+fromSelect.innerHTML = optionsHTML;
+toSelect.innerHTML = optionsHTML;
